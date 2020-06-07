32c65ef8af061a67a1fd5e9c1270f47db125b580
Alex Lerner <alex_lerner@troweprice.com>
2020-06-07 11:20:51 -0400

    Initial Commit -- generated a WS that registers with Eureka Discovery service

---
+++ src/test/java/com/pragmaticbitbucket/photoapp/api/account/PhotoappapiaccountmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragmaticbitbucket.photoapp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappapiaccountmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
